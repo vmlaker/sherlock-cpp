05398acb18b10cb7d8b39862d1c5eff910d9493d
vmlaker <velimir.mlaker@gmail.com>
2013-11-23 09:52:24 -0800

    Fix deallocation, move display to separate thread, cleanup code.

--- src/diffavg3.cpp
+++ src/diffavg3.cpp
@@ -1,21 +1,27 @@
 // Difference from running average, with multiprocessing.
 
+// Include standard headers.
 #include <vector>
 #include <sstream>
 #include <thread>
+
+// Include 3rd party headers.
 #include <boost/date_time.hpp>
 #include <opencv2/opencv.hpp>
 #include "RateTicker.hpp"
 #include "ConcurrentQueue.hpp"
+
+// Include application headers.
 #include "util.hpp"
+
 
 // Capture frames for given *duration* number of seconds,
 // and push frames and alpha values onto their respective queues.
-void step1(
+void capture(
     cv::VideoCapture* cap,
     const int& duration, 
-    ConcurrentQueue< cv::Mat* >* frames,
-    ConcurrentQueue< float >* alphas
+    ConcurrentQueue <cv::Mat*>* captures,
+    ConcurrentQueue <float>* alphas
     )
 {
     // Keep track of previous iteration's timestamp.
@@ -27,7 +33,7 @@ void step1(
     auto end = now + dur;
     while (end > boost::posix_time::microsec_clock::universal_time())
     {
-        // Take a snapshot.
+        // Capture the snapshot.
         auto frame = new cv::Mat;
         *cap >> *frame; 
 
@@ -35,22 +41,22 @@ void step1(
         auto alpha = sherlock::getAlpha(tstamp_prev, 1.0);
 
         // Push the frame and alpha value onto respective queues.
-        frames->push(frame);
+        captures->push(frame);
         alphas->push(alpha);
     }
 
     // Signal end-of-processing by pushing NULL value onto the queue.
-    frames->push(NULL);
+    captures->push(NULL);
 }
 
 
 // Compute Difference from Average of the frames,
 // taking into account previously computed alpha values,
 // and push resulting diff frames onto the queue.
-void step2(
-    ConcurrentQueue< cv::Mat* >* frames,
-    ConcurrentQueue< cv::Mat* >* diffs,
-    ConcurrentQueue< float >* alphas
+void diff_average(
+    ConcurrentQueue <cv::Mat*>* captures,
+    ConcurrentQueue <cv::Mat*>* diffs,
+    ConcurrentQueue <float>* alphas
     )
 {
     // Monitor framerates for the given seconds past.
@@ -62,7 +68,7 @@ void step2(
 
     // Pull from the queue while there are valid frames.
     cv::Mat* frame;
-    frames->wait_and_pop(frame);
+    captures->wait_and_pop(frame);
     while(frame){
 
         const auto RTYPE = CV_32FC3;
@@ -97,27 +103,85 @@ void step2(
         // Convert the diff image.
         diff->convertTo(*diff, frame->type());
 
+        // Original frame is no longer needed, so deallocate it.
+        delete frame;
+
         // Write the processing framerate on top of the diff image.
         auto fps = framerate.tick();
         std::ostringstream line;
         line << std::fixed << std::setprecision(2);
         line << fps[0] << ", " << fps[1] << ", " << fps[2] << " (processing)";
-        std::list<std::string> lines;
-        lines.push_back(line.str());
+        std::list<std::string> lines ({ line.str() });
         sherlock::writeOSD(*diff, lines, 0.04);
 
         // Push diff image onto queue.
         diffs->push(diff);
 
-        // Original frame is no longer needed, so deallocate it.
-        delete frame;
-
         // Retrieve the next frame from queue.
-        frames->wait_and_pop(frame);
+        captures->wait_and_pop(frame);
     }
 
     // Signal end-of-processing by pushing NULL value onto the queue.
     diffs->push(NULL);
+}
+
+
+// Display queued frames.
+void display(
+    ConcurrentQueue <cv::Mat*>* diffs
+    )
+{
+    // Create the output window.
+    const char* title = "diff average 2";
+    cv::namedWindow(title, CV_WINDOW_NORMAL);
+
+    // Monitor framerates for the given seconds past.
+    std::vector<float> periods = { 1, 5, 10 };
+    RateTicker framerate (periods);
+
+    // Pull from the queue while there are valid matrices.
+    cv::Mat* frame;
+    diffs->wait_and_pop(frame);
+    while(frame){
+
+        // Write the display framerate on top of the image.
+        auto fps = framerate.tick();
+        std::ostringstream line;
+        line << std::fixed << std::setprecision(2);
+        line << fps[0] << ", " << fps[1] << ", " << fps[2] << " (display)";
+        // Show the display framerate *below* the processing framerate
+        // by first adding a "carriage return" (empty line) to the list.
+        std::list<std::string> lines({ "", line.str() });
+        sherlock::writeOSD(*frame, lines, 0.04);
+
+        // Display the snapshot.
+        cv::imshow(title, *frame);
+
+        // Allow HighGUI to process event.
+        cv::waitKey(1);
+
+        // Deallocate the current image and retrieve the next,
+        // while filtering out excess (intermediate) images in the queue.
+        // If display hardware is not fast enough, 
+        // showing intermediate images introduces (incremental) lag.
+        // Hence the "lossy filter" by dropping excess frames.
+        cv::Mat* prev = frame;
+        int count = 0;
+        while(diffs->try_pop(frame))
+        {
+            // Since we popped the next image, we can delete the previous.
+            delete prev; 
+            prev = frame; 
+            count++;
+        }
+        // If there weren't any successful try-pops above,
+        // then do an absolute wait-and-pop here.
+        if(count == 0)
+        {
+            diffs->wait_and_pop(frame);
+            delete prev;
+        }
+    }
 }
 
 
@@ -137,65 +201,18 @@ int main(int argc, char** argv)
     cap.set(3, WIDTH);
     cap.set(4, HEIGHT);
 
-    // Create the output window.
-    const char* title = "diff average 2";
-    cv::namedWindow(title, CV_WINDOW_NORMAL);
-
-    // Monitor framerates for the given seconds past.
-    std::vector<float> periods = { 1, 5, 10 };
-    RateTicker framerate (periods);
-
     // Create the shared queues.
-    ConcurrentQueue <cv::Mat*> frames;
+    ConcurrentQueue <cv::Mat*> captures;
     ConcurrentQueue <float> alphas;
     ConcurrentQueue <cv::Mat*> diffs;
 
     // Start up the threads.
-    std::thread thread1 (step1, &cap, DURATION, &frames, &alphas);
-    std::thread thread2 (step2, &frames, &diffs, &alphas);
+    std::thread capturer (capture, &cap, DURATION, &captures, &alphas);
+    std::thread diff_averager (diff_average, &captures, &diffs, &alphas);
+    std::thread displayer (display, &diffs);
 
-    // Pull from the queue while there are valid matrices.
-    cv::Mat* diff;
-    diffs.wait_and_pop(diff);
-    while(diff){
-
-        // Write the display framerate on top of the image.
-        auto fps = framerate.tick();
-        std::ostringstream line;
-        line << std::fixed << std::setprecision(2);
-        line << fps[0] << ", " << fps[1] << ", " << fps[2] << " (display)";
-        std::list<std::string> lines;
-        //
-        // Show the display framerate *below* the processing framerate
-        // by first adding a "carriage return" (empty line) to the list.
-        lines.push_back("");
-        //
-        lines.push_back(line.str());
-        sherlock::writeOSD(*diff, lines, 0.04);
-
-        // Display the snapshot.
-        cv::imshow(title, *diff);
-
-        // Allow HighGUI to process event.
-        cv::waitKey(1);
-        
-        // Deallocate the current image.
-        delete diff;
-
-        // Filter out excess images in the queue.
-        // If display hardware is not fast enough, showing these images 
-        // would introduce (incremental) lag, therefore we add
-        // lossy filtering here by dropping any excess frames.
-        while(diffs.size()>1){
-            diffs.wait_and_pop(diff);
-            delete diff;
-        }
-
-        // Finally, pop off the next image.
-        diffs.wait_and_pop(diff);
-    }
-
-    // Join the two forked threads.
-    thread1.join();
-    thread2.join();
+    // Join the threads.
+    capturer.join();
+    diff_averager.join();
+    displayer.join();
 }
